8d7fdc8638e3e2a83e0e99255b79248df4cbd8e5
Lucy Blue <lucyblueglass@gmail.com>
2016-06-10 15:53:15 -0700

    basic html and extablishes variables in js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(function(){
+
+var location = $("#location").val();
+var food = $("#food").val();
+var transportation = $("#transportation").val();
+var lodging = $("#lodging").val();
+var nighttime = $("#nighttime").val();
+
+});
