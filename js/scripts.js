4e8ab7f6893c94a4f1641051d7fffbd9feff7e2d
Lucy Blue <lucyblueglass@gmail.com>
2016-06-11 10:51:18 -0700

    front end logic for questionaire

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,19 @@ $(function(){
     var daytime = parseInt($("#daytime").val());
     var location = parseInt($("#location").val());
     var food = parseInt($("#food").val());
-    var transportation = parseInt($("#transportation").val());
-    var lodging = parseInt($("#lodging").val());
-    var nighttime = parseInt($("#nighttime").val());
+    var transportation = parseInt($("input:radio[name=transportation]:checked").val());
+    var lodging = parseInt($("input:radio[name=lodging]:checked").val());
+    
+    var destination = daytime + location + food + transportation + lodging;
 
-    var destination = daytime + location + food;
-
-    if (destination <= 5) {
+    if (destination <= 17) {
       $("#hawaii").show();
-    } else if (destination >= 6) {
+    } else if (destination >= 18 && destination <= 23){
       $("#rockymountains").show();
+    } else if (destination >= 24  && destination <= 28){
+      $("#newyork").show();
+    } else if (destination >= 29 && destination <=32){
+      $("#losangeles").show();
     }
 
     event.preventDefault();
